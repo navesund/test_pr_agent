c5d9eed27c6d7b806517137bfdcf1b1ef7e006e2
navesund <naveensundarrajan@gmail.com>
2025-04-28 17:20:58 +0530

    Update sample.java

--- src/main/java/sample.java
+++ src/main/java/sample.java
@@ -2,8 +2,6 @@ package main.java;
 
 public class sample {
     public static void main(String[] args) {
-        int value1 = 10, value2=0;
-        int result = value1 / value2;
-        System.out.println("Result: " + result);
+        int result = 10 / 0;
     }
 }
