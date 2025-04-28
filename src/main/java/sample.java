87c24e49a90cfd61ed438aa1c55f25ab36ee185e
navesund <naveensundarrajan@gmail.com>
2025-04-28 12:08:14 +0530

    Create sample.java

---
+++ src/main/java/sample.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class sample {
+    public static void main(String[] args) {
+        int value1 = 10, value2=0;
+        int result = value1 / value2;
+        System.out.println("Result: " + result);
+    }
+}
