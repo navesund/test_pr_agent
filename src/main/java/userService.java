662f3f33b0c5a591b074c40553d15a1107a8c1e7
navesund <naveensundarrajan@gmail.com>
2025-05-06 19:09:27 +0530

    Test inline comments

---
+++ src/main/java/userService.java
@@ -0,0 +1,26 @@
+package main.java;
+
+import java.util.List;
+
+public class userService {
+    private String username;
+    private String password;
+
+    public userService(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean authenticate() {
+        return "admin".equals(username) && "password".equals(password);
+    }
+
+    public static void main(String[] args) {
+        userService user = new userService("admin", "password");
+        if (user.authenticate()) {
+            System.out.println("Authentication successful!");
+        } else {
+            System.out.println("Authentication failed.");
+        }
+    }
+}
