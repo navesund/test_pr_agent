d23430f1d0ca308b2885f94559dbfa314cc64bce
navesund <naveensundarrajan@gmail.com>
2025-05-08 19:21:39 +0530

    Create calculator.java

---
+++ src/main/java/calculator.java
@@ -0,0 +1,26 @@
+package main.java;
+
+public class calculator {
+
+    // This is a simple calculator class that performs basic arithmetic operations
+
+    //Performs addition and returns the result
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    //Performs subtraction and returns the result
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    //Performs multiplication and returns the result
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    //Performs division and returns the result
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
