b00b6aea1c9edf684c0e1e852e7767f4f1dfd89f
navesund <naveensundarrajan@gmail.com>
2025-05-12 17:46:14 +0530

    Update customRules.java

--- src/main/java/customRules.java
+++ src/main/java/customRules.java
@@ -2,6 +2,7 @@ package main.java;
 
 public class customRules {
     public static void main(String[] args) {
+        // Test custom rules
         System.out.println("Class to test custom rules");
     }
 }
