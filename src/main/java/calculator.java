7d8ce1b3dad1a351a9ffea63fc4f592fb0dc1c5d
navesund <naveensundarrajan@gmail.com>
2025-05-12 16:15:15 +0530

    Update calculator.java

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -24,6 +24,17 @@ public class calculator {
         return a / b;
     }
 
+    //Performs modulus and returns the result
+    public int modulus(int a, int b) {
+        return a % b;
+    }
+
+    //Calculates percentage and returns the result
+    public double percentage(int a, int b) {
+       double result = (b * 100 )/ a;
+       return result;
+    }
+
     //Main method to test the calculator class
     public static void main(String[] args) {
         calculator calc = new calculator();
@@ -31,5 +42,7 @@ public class calculator {
         System.out.println("Subtraction: " + calc.subtract(5, 3));
         System.out.println("Multiplication: " + calc.multiply(5, 3));
         System.out.println("Division: " + calc.divide(5, 0));
+        System.out.println("Modulus: " + calc.modulus(5, 3));
+        System.out.println("Percentage: " + calc.percentage(5, 3));
     }
 }
